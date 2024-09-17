974cce9c863824cb34c0de001199ff9d3cf4df17
Yohanann Andrianasolo <yohan@10.3.61.206>
2024-09-17 12:59:13 +0200

    Création d'un nouveau projet versionné

---
+++ tp0_exo2/src/tp0_exo2/Tp0_exo2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_exo2;
+
+/**
+ *17/09/2024
+ * @author yohan
+ */
+public class Tp0_exo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
