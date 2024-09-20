ba59f1cc4b0df54efa5b460556e7096dae193bdb
Yohanann Andrianasolo <yohan@10.3.61.206>
2024-09-20 17:01:25 +0200

    Création d'un nouveau projet versionné

--- tp0_exo2/src/tp0_exo2/Tp0_exo2.java
+++ tp0_exo2/src/tp0_exo2/Tp0_exo2.java
@@ -4,6 +4,8 @@
  */
 package tp0_exo2;
 
+import java.util.Scanner;
+
 /**
  *17/09/2024
  * @author yohan
@@ -11,10 +13,28 @@ package tp0_exo2;
 public class Tp0_exo2 {
 
     /**
-     * @param args the command line arguments
+     * Andrianasolo Yohanann
+     * 20/09/2024
+     * 
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        int nb;
+        int result;     
+        Scanner sc = new Scanner (System.in);
+        System.out.println("\nEntrer le nombre : ");
+        nb = sc.nextInt();      
+        int ind;
+        
+        result = 0;
+        
+
+        ind  = 1;
+        while (ind <= nb){
+ 
+            result = result+ind;
+            ind = ind + 1;
+        }
+        System.out.println();
+        System.out.println("La somme des " + nb + "entiers est: " +result);
+}
 }
